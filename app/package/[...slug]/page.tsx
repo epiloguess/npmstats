ea2d074cc2452072717db3dd48ba8237f9562654
epiloguess <hellowyho@gmail.com>
2024-03-24 19:17:52 +0800

    delete useless Parallel Routes

--- app/package/[...slug]/page.tsx
+++ app/package/[...slug]/page.tsx
@@ -1,10 +1,12 @@
-import { Result } from "antd";
 import { getPkgTag } from "../../_libs/func";
 
-async function getNpmMeta(pkg_name:string) {
+import Cnpm from "./Cnpm";
+import Npm from "./Npm";
+async function getNpmMeta(pkg_name: string) {
+  const res = await fetch(
+    `https://registry.npmjs.org/-/v1/search?text=${pkg_name}`
+  );
 
-  const res = await fetch(`https://registry.npmjs.org/-/v1/search?text=${pkg_name}`)
-  
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch NpmMeta");
@@ -12,26 +14,33 @@ async function getNpmMeta(pkg_name:string) {
   return res.json();
 }
 
-
 export default async function Page({ params }: { params: { slug: string[] } }) {
   const undecodedString = params.slug.join("/");
   const pkg_name = decodeURIComponent(undecodedString);
 
   let tags = getPkgTag(pkg_name);
-  let {objects:npm_meta} =  await getNpmMeta(pkg_name)
-  let {package:real_meta} = npm_meta.find((e)=>(e.package.name === pkg_name))
+  let { objects: npm_meta } = await getNpmMeta(pkg_name);
+  let { package: real_meta } = npm_meta.find(
+    (e) => e.package.name === pkg_name
+  );
 
   return (
-    <div className="flex flex-col gap-2">
-      <div className="py-2">
-        <a className=" text-orange-500 text-2xl font-bold " href={`https://www.npmjs.com/package/${pkg_name}`}>
-          {pkg_name}
-        </a>
+    <div className='flex flex-col '>
+      <div className=''>
+        <h2>
+          <a
+            className=' text-orange-500 text-2xl font-bold '
+            href={`https://www.npmjs.com/package/${pkg_name}`}>
+            {pkg_name}
+          </a>
+        </h2>
       </div>
-      <div className="flex gap-2 flex-wrap">
+      <div className='flex gap-2 flex-wrap'>
         {tags &&
           tags.map((tag) => (
-            <div key={tag} className=" bg-gray-300 hover:bg-gray-400  px-2 rounded">
+            <div
+              key={tag}
+              className=' bg-gray-300 hover:bg-gray-400  px-2 rounded'>
               <p>
                 <a href={`/tags/${tag}`}>{tag}</a>
               </p>
@@ -40,6 +49,14 @@ export default async function Page({ params }: { params: { slug: string[] } }) {
       </div>
       <div>{real_meta.description}</div>
 
+      <div className='md:flex gap-2'>
+        <div className='md:w-1/2'>
+          <Cnpm params={params}></Cnpm>
+        </div>
+        <div className='md:w-1/2'>
+          <Npm params={params}></Npm>
+        </div>
+      </div>
     </div>
   );
 }
--- app/package/layout.tsx
+++ app/package/layout.tsx
@@ -2,10 +2,7 @@ export default function Layout({ children, npm, cnpm }) {
   return (
     <>
       {children}
-      <div className="md:flex gap-2">
-        <div className="md:w-1/2">{cnpm}</div>
-        <div className="md:w-1/2">{npm}</div>
-      </div>
+
     </>
   );
 }
