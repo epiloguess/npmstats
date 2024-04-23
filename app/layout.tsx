2761197b72ea7937fbe07343af6d05fa27c462c6
epiloguess <hellowyho@gmail.com>
2024-04-23 16:31:11 +0800

    add umami support

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import SearBox from "@/_component/SearchBox";
 const inter = Inter({ subsets: ["latin"] });
-
+import Script from "next/script";
 export const metadata: Metadata = {
   title: "Npm Stats",
   description:
@@ -23,8 +23,8 @@ export default function RootLayout({
         className={`${inter.className} max-w-screen-xl m-auto px-6 md:px-24  pb-10 `}>
         <Nav />
         <SearBox></SearBox>
-
         {children}
+        <Script src="https://analytics.wunhao.com/script.js" data-website-id="f1d46e25-f881-421e-a09f-bcbdb4f27b3d" strategy="lazyOnload"></Script>
       </body>
     </html>
   );
