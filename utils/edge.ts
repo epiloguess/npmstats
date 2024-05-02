c8dd9afc1600e7e31ea44650230be2ff57cdc911
epiloguess <hellowyho@gmail.com>
2024-05-02 17:00:54 +0800

    fix bug

--- utils/edge.ts
+++ utils/edge.ts
@@ -1,6 +1,3 @@
-import { getRequestContext } from "@cloudflare/next-on-pages";
-import { PrismaClient } from "@prisma/client";
-import { PrismaD1 } from "@prisma/adapter-d1";
 
 export const runtime = "edge";
 
