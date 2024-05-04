39245dec34e1e6a1bb302997f46bf6c1d788f518
epiloguess <hellowyho@gmail.com>
2024-05-05 00:46:27 +0800

    add some suspense

--- components/PkgMeta.tsx
+++ components/PkgMeta.tsx
@@ -3,15 +3,15 @@ import Npm_logo from "@public/npm-logo.svg";
 import Github_logo from "@public/github-mark.svg";
 import { Package } from "@type/npm";
 import Link from "next/link";
+import { getPkgMeta } from "@utils/server";
 
 export async function PkgMeta({ meta }: { meta: Package }) {
-  let { pkg, description, repository } = meta;
+  let { pkg,description, repository } = meta;
   return (
     <div className='flex flex-col gap-2'>
       <div className='flex items-center gap-2'>
-        <Link href={`/package/${pkg}`}>
-        <h2 className=' text-orange-500 text-xl font-bold '>{pkg}</h2>
-
+        <Link href={`/package/${pkg}`} >
+          <h2 className=' text-orange-500 text-xl font-bold '>{pkg}</h2>
         </Link>
         <a target='_blank' href={`https://www.npmjs.com/package/${pkg}`}>
           <Npm_logo width='24' height='24'></Npm_logo>
